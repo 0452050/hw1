59a1b196d8854ab22d14b00473e42768db6c888e
0452050 <0452050@nkust.edu.tw>
2018-05-18 16:06:37 +0800

    third commit

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a=1;
-	int b=2;
-	int sum;
+	int variable1=1;//跑计1 
+	int variable2=2;//跑计2 
+	int sum;//羆㎝ 
 	
-	sum=a+b;
-	printf("sum=%d",sum);
+	sum=variable1+variable2;//羆㎝跑计1+跑计2 
+	printf("sum=%d",sum);//羆㎝ 
 	return 0;
 }
