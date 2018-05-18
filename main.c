41462a26c60492afda20c142ef0b60676c081a50
0452050 <0452050@nkust.edu.tw>
2018-05-18 16:01:47 +0800

    second commit

--- main.c
+++ main.c
@@ -4,6 +4,11 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("hello teacher");
+	int a=1;
+	int b=2;
+	int sum;
+	
+	sum=a+b;
+	printf("sum=%d",sum);
 	return 0;
 }
